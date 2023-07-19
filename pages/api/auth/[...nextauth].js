fe9ae95326c24792ac51e2a0e94ab1d356320cce
Christian Martinez <christian@mixdlabs.com>
2023-07-19 17:39:03 -0600

    Began hashing passwords. Not finished yet. Successfully hashing passwords in the db for now

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import { compare } from 'bcryptjs'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from 'next-auth/providers/credentials'
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,6 @@
 import connectToMongoDb from '@/lib/mongodb'
 import User from '@/models/user'
+import { hash } from 'bcryptjs'
 
 const signUpHandler = async (req, res) => {
   connectToMongoDb().catch((err) => res.json(err))
@@ -13,37 +14,38 @@ const signUpHandler = async (req, res) => {
 
     if (userExists) {
       return res.status(409).json({ error: 'User Already exists' })
-    } else {
-      if (password.length < 6)
-        return res
-          .status(409)
-          .json({ error: 'Password should be 6 characters long' })
-
-      User.create({
-        fullName,
-        email,
-        password,
-        language,
-        country,
-      })
-        .then((data) => {
-          const user = {
-            fullName: data.fullName,
-            email: data.email,
-            password: data.password,
-            language: data.language,
-            country: data.country,
-            _id: data._id,
-          }
-          return res.status(201).json({
-            success: true,
-            user,
-          })
-        })
-        .catch((err) => {
-          res.status(409).json({ error: err })
-        })
+    } else if (password.length < 6) {
+      return res
+        .status(409)
+        .json({ error: 'Password should be 6 characters long' })
     }
+
+    const hashedPassword = await hash(password, 12)
+
+    User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      language,
+      country,
+    })
+      .then((data) => {
+        const user = {
+          fullName: data.fullName,
+          email: data.email,
+          password: data.password,
+          language: data.language,
+          country: data.country,
+          _id: data._id,
+        }
+        return res.status(201).json({
+          success: true,
+          user,
+        })
+      })
+      .catch((err) => {
+        res.status(409).json({ error: err })
+      })
   } else {
     res.status(401).json('Unauthorized')
   }
